6b94f7b1b022270a91ba85922a0ea2ebc1168faf
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-22 11:04:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,86 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # # . . .
+            # # # . #
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # # . # .
+            # # # . .
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            # . # . .
+            # # . . .
+            # # # . .
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . # . . .
+            # . . . .
+            # # . . .
+            # # # . .
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # # . . .
+            # # # . .
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . # . . .
+            # . . . .
+            # # . . .
+            # # # . .
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            # . # . .
+            # # . . .
+            # # # . .
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # # . # .
+            # # # . .
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # # . . .
+            # # # . #
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # # . . .
+            # # # . .
+            # # # # .
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+        basic.pause(500)
+    }
 })
